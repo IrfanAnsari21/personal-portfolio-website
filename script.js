e6c973a3980739550daa0a040d4696b5509be863
IrfanAnsari21 <irfanansari.career@gmail.com>
2026-05-01 20:47:28 +0530

    made the website responsive

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const tablinks = document.getElementsByClassName("tab-links");
 const tabContents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname){
-    for(tablink of tablinks){
+function opentab(tabname) {
+    for (tablink of tablinks) {
         tablink.classList.remove("active-link");
     }
-    for(tabContent of tabContents){
+    for (tabContent of tabContents) {
         tabContent.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
+}
+
+// ------------ Menu ------------
+const sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
 }
