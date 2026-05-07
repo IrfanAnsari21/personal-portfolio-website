2e8360438d3c3f6f3baed2dd3c79a46e0afc47b7
IrfanAnsari21 <irfanansari.career@gmail.com>
2026-05-07 21:41:11 +0530

    new project added and made see more button interactive

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const tablinks = document.getElementsByClassName("tab-links");
 const tabContents = document.getElementsByClassName("tab-contents");
+const projectBtn = document.getElementById("projectBtn");
+const hideWork = document.querySelectorAll(".hideWork");
 
+// ---------- About Me ----------
 function opentab(tabname) {
     for (tablink of tablinks) {
         tablink.classList.remove("active-link");
@@ -11,6 +14,19 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+// ---------- See More button ----------
+projectBtn.addEventListener("click", () => {
+    hideWork.forEach((el) => {
+        if (el.classList.contains("active")) {
+            el.classList.remove("active");
+            projectBtn.innerText = "See More"
+        } else {
+            el.classList.add("active");
+            projectBtn.innerText = "Show Less"
+        }
+    });
+});
 
 // ------------ Menu ------------
 const sidemenu = document.getElementById("sidemenu");
